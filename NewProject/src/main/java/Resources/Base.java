73e2cb3285ed6d610c6a7d8f8392c2f8d61a735d
Anubhav Koul <anubhavcgc.mca11@gmail.com>
2022-02-26 02:03:01 +0530

    Enhanced the base method along with maven with cmd
    1. Added Maven surefire plugin to trigger test using testng from cmd.
    2. Tweaked Base class for clean code and consistency.
    3. Removed one unused class.
    4.

--- NewProject/src/main/java/Resources/Base.java
+++ NewProject/src/main/java/Resources/Base.java
@@ -7,6 +7,8 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 
 public class Base {
@@ -29,10 +31,27 @@ public class Base {
 			System.setProperty("webdriver.chrome.driver", "C:\\My Data\\GIT Repo\\Dependency\\chromedriver.exe");
 			driver = new ChromeDriver();
 		}
+		
+		else if(browserName.equalsIgnoreCase("IE"))
+		{
+			System.setProperty("webdriver.chrome.driver", "C:\\My Data\\GIT Repo\\Dependency\\IEdriver.exe");
+			driver = new InternetExplorerDriver();
+		}
+		
+		else if(browserName.equalsIgnoreCase("Edge"))
+		{
+			System.setProperty("webdriver.chrome.driver", "C:\\My Data\\GIT Repo\\Dependency\\Edgedriver.exe");
+			driver = new EdgeDriver();
+		}
 
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 		return driver;
+	}
+	
+	public String getUrl()
+	{
+		return prop.getProperty("url");
 	}
 
 }
--- NewProject/src/test/java/Framework/NewProject/AppTest.java
+++ NewProject/src/test/java/Framework/NewProject/AppTest.java
@@ -1,6 +1,8 @@
 package Framework.NewProject;
 import java.io.IOException;
 import org.apache.logging.log4j.*;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import Resources.Base;
 import pageObjects.PageName;
@@ -9,13 +11,27 @@ import pageObjects.PageName;
 public class AppTest extends Base {
 	private static Logger log = LogManager.getLogger(AppTest.class.getName());
 
+	@BeforeTest
+	public void StartUp() throws IOException
+	{
+		/*Initialize browser and hitting Url will be set up here.*/
+		driver = initializeBrowser();
+		String url = getUrl();
+		driver.get(prop.getProperty("url"));
+	}
+	
 	@Test
 	public void navigateToAnubhavHomePage() throws IOException, InterruptedException {
-		driver = initializeBrowser();
-		driver.get(prop.getProperty("url"));
-		
+
 		PageName pageObject = new PageName(driver);
 		pageObject.clickContactUs();
 		
+	}
+	
+	@AfterTest
+	public void tearDown()
+	{
+		/*Closing the browser, connections etc would be set here.*/
+		driver.close();
 	}
 }
